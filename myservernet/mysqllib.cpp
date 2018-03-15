917632bfb64ff71ff686052ef7313c14c62f40cc
Your Name <you@example.com>
2018-03-15 16:43:10 +0800

    20183151643

---
+++ myservernet/mysqllib.cpp
@@ -0,0 +1,65 @@
+#include "mysqllib.h"
+
+mysqllib * mysqllib::instance = new mysqllib;
+
+mysqllib::mysqllib()
+{
+	mysql = mysql_init(NULL);
+	mysql_real_connect(mysql, "127.0.0.1", "root", "123456", "webnet", 3306, NULL, 0);
+	if (NULL == mysql)
+	{
+		cout << "connect error" << endl;
+	}
+	mysql_query(mysql, "set names utf8");
+}
+
+mysqllib::mysqllib(mysqllib&)
+{
+}
+
+mysqllib::~mysqllib()
+{
+	mysql_close(mysql);
+}
+
+mysqllib * mysqllib::getInstance()
+{
+	return instance;
+}
+
+int mysqllib::insert(char *sql)
+{
+	return mysql_query(mysql, sql);
+}
+
+int mysqllib::select(char *sql)
+{
+	mysql_query(mysql, sql);
+	MYSQL_RES *result = mysql_store_result(mysql);
+	printf("result.\n");
+	int rows = mysql_num_rows(result);
+	if (rows)
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int mysqllib::select(char *sql, char *result_buf)
+{
+	if(mysql_query(mysql, sql))
+	{
+		return -1;
+	}
+	MYSQL_RES *result = mysql_store_result(mysql);
+	MYSQL_ROW row;
+	while (row = mysql_fetch_row(result))
+	{
+		sprintf(result_buf, "%s%s\n", result_buf, row[0]);
+	}
+	return 0;
+}
+
